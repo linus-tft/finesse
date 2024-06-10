efcbd9475ac1fbe8348d208cccfdbe6ec60f6684
Sunil Chaware <chawaresunil1987@gmail.com>
2024-06-10 22:49:13 +0530

    Basic structure of the project

---
+++ Finesse.TaskApp/Finesse.Common/Models/TaskDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finesse.Common.Models
+{
+    public class TaskDetails
+    {
+        public int TaskId { get; set; }
+        public string TaskDescription { get; set; } = string.Empty;
+        public string TaskName { get; set; } = string.Empty;
+        public string TaskStatus { get; set; }= string.Empty;
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set;}
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+
+    }
+}
